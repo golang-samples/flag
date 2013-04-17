8c573c4719bb761e9b0b72e725ffaa25ba83795d
ryochack <ryochack@gmail.com>
2013-04-18 02:18:07 +0900

    add visit

---
+++ visit/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	f := flag.NewFlagSet("flag", flag.ExitOnError)
+	_ = f.Bool("bool", false, "this is bool flag")
+	_ = f.Int("int", 0, "this is int flag")
+
+	visitor := func(a *flag.Flag) {
+		fmt.Println(">", a.Name, "value=", a.Value)
+	}
+
+	fmt.Println("Visit()")
+	f.Visit(visitor)
+	fmt.Println("VisitAll()")
+	f.VisitAll(visitor)
+
+	// set flags
+	f.Parse([]string{"-bool", "-int", "100"})
+
+	fmt.Println("Visit() after Parse()")
+	f.Visit(visitor)
+	fmt.Println("VisitAll() after Parse()")
+	f.VisitAll(visitor)
+}
